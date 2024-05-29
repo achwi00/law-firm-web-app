056079b9c31cb8768929781688802a500128aa86
achwi00 <achwierut00@gmail.com>
2024-05-29 18:54:03 +0200

    Spring security added, index available after auth

--- lawfirm/src/main/java/org/lawfirm/config/SecurityConfig.java
+++ lawfirm/src/main/java/org/lawfirm/config/SecurityConfig.java
@@ -1,29 +1,35 @@
-//package org.lawfirm.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//import static org.springframework.security.config.Customizer.withDefaults;
-//
-//
-//@Configuration
-//public class SecurityConfig {
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests((authz) -> authz
-//                        .anyRequest().authenticated()
-//                )
-//                .httpBasic(withDefaults());
-//        return http.build();
-//    }
-//
-//
-//}
+package org.lawfirm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                // Allow access to "/public" endpoint without authorization
+                        .requestMatchers("/myAccount").authenticated()
+                    .requestMatchers("/home", "/login", "register").permitAll()
+                // Other requests require authentication
+                .anyRequest().authenticated()
+
+        )
+
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+
+}
 
 /*@EnableWebSecurity
 public class SecurityConfig {
