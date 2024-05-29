b1cbda0e5ed01e1475fe30abfa25d14ca0ff98e6
achwi00 <achwierut00@gmail.com>
2024-05-29 17:57:47 +0200

    Correct view location found

---
+++ lawfirm/src/main/java/org/lawfirm/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+//package org.lawfirm.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import static org.springframework.security.config.Customizer.withDefaults;
+//
+//
+//@Configuration
+//public class SecurityConfig {
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((authz) -> authz
+//                        .anyRequest().authenticated()
+//                )
+//                .httpBasic(withDefaults());
+//        return http.build();
+//    }
+//
+//
+//}
+
+/*@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .authorizeHttpRequests((requests) -> requests
+                        .anyRequest().authenticated()
+                )
+                .formLogin(login -> login
+                        .loginPage("/login-page")
+                        .permitAll())
+                .logout(logout -> logout.permitAll())
+                .build();
+    }
+
+    // Additional security configuration methods (e.g., user details service)
+}*/
---
+++ lawfirm/src/main/java/org/lawfirm/controller/WebController.java
@@ -0,0 +1,24 @@
+package org.lawfirm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+    @RequestMapping("/home")
+    public String home() {
+        return "example.html";
+    }
+
+//    @RequestMapping("/home")
+//    public String home() {
+//        return "index.html";
+//    }
+//
+//    @GetMapping("/login-page")
+//    public String login() {
+//        return "login-page.html";
+//    }
+}
